c30444b6c6f4421187fe4b9a688ef86c3cf00297
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2023-01-16 18:08:17 +0500

    I added a new addition to my work on objects

--- homework-for-object/homework1.js
+++ homework-for-object/homework1.js
@@ -4,10 +4,15 @@ this.myname = prompt('ismingizni kiriting', 'Lutfulla');
 this.yourname = name;
 this.hisname = prompt('ismingizni kiriting', "Alex");
 this.hername = prompt('ismingizni kiriting', "Alica");
-
+this.newObj = acceptable = {
+  that: "female"
+};
   }
   let Names = new users('maks');
   
   
   console.log(Names.myname); 
   console.log(Names.yourname);
+  Names.isAdmin = true;
+  console.log(Names);
+  let phrase = `if i set thisb word ${Names.newObj}`
