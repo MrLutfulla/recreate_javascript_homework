3cfdf27e3361b495443a19d448e5225ed877f78a
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2023-01-20 17:33:32 +0500

    thanks for watching

--- homework-for-object/homework2.js
+++ homework-for-object/homework2.js
@@ -6,43 +6,43 @@ birnchi o'zgaruvchini kiritamiz u bilan qilinadigan amalni tanlaymiz va ikkinchi
 //     expres2 = +prompt('ikkinchi operatorni kiriting');
 
 
-// if((String(expres1) === 'NaN')||(String(expres2) === 'NaN')){
-// 	console.log('iltimos son kiriting')
-// // agar operand teng bulmasa + yoki op =bulmasa - yoki =bulmasa *yoki = bulmasa /  
-// }else if(operand !== '+' && operand !== '*' && operand !== '/' && operand !== '-') {
-//     console.log('siz boshqa narsa kiritdingiz qayta kiriting');
+if((String(expres1) === 'NaN')||(String(expres2) === 'NaN')){
+	console.log('iltimos son kiriting')
+// agar operand teng bulmasa + yoki op =bulmasa - yoki =bulmasa *yoki = bulmasa /  
+}else if(operand !== '+' && operand !== '*' && operand !== '/' && operand !== '-') {
+    console.log('siz boshqa narsa kiritdingiz qayta kiriting');
     
-// }else if(operand == '/' || '*' || '+' || '-'){
+}else if(operand == '/' || '*' || '+' || '-'){
     
-//     if(operand == '/'){
-//         console.log(expres1 / expres2);
-//     }else if(operand == '+'){
-//         console.log(expres1 + expres2);
-//     }else if(operand == '-'){
-//         console.log(expres1 - expres2);
-//     }else if(operand == '*'){
-//         console.log(expres1 * expres2);
-//     }
+    if(operand == '/'){
+        console.log(expres1 / expres2);
+    }else if(operand == '+'){
+        console.log(expres1 + expres2);
+    }else if(operand == '-'){
+        console.log(expres1 - expres2);
+    }else if(operand == '*'){
+        console.log(expres1 * expres2);
+    }
 
     
-// }
+}
 
-// switch(operand){
-// 	case "+":
-// 		console.log(expres1 + expres2)
-// 		break
-// 	case "-":
-// 		console.log(expres1 - expres2)
-// 		break
-// 	case "*":
-// 		console.log(expres1 * expres2)
-// 		break
-//         case "/":
-// 		console.log(expres1 / expres2)
-// 		break
-// 	default:
-// 		console.log('error')
-// }
+switch(operand){
+	case "+":
+		console.log(expres1 + expres2)
+		break
+	case "-":
+		console.log(expres1 - expres2)
+		break
+	case "*":
+		console.log(expres1 * expres2)
+		break
+        case "/":
+		console.log(expres1 / expres2)
+		break
+	default:
+		console.log('error')
+}
         
 
 
